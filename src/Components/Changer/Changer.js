d84b3c8c5506fe2a68860c76a4cac0570d680eac
Krishna <krishnakumarkush@gmail.com>
2023-04-29 12:55:38 +0530

    changer frontend updated

--- src/Components/Changer/Changer.js
+++ src/Components/Changer/Changer.js
@@ -3,14 +3,18 @@ import React from 'react'
 import ChangerBlocks from './ChangerBlocks'
 
 const Changer = () => {
-  let elements = ["Feed", "My Questions", "Circle"]
+  let elements = [
+    ["Feed", true],
+    ["My Questions", false],
+    ["Circle", false]
+  ]
 
   return (
     <div id="changer-container">
       <div id="changer">
       {elements.map((e, i) => {
         return (
-          <ChangerBlocks id={i} ele={e}/>
+          <ChangerBlocks id={i} name={e[0]} isSelected={e[1]}/>
           )
         })}
       </div>
--- src/Components/Changer/ChangerBlocks.js
+++ src/Components/Changer/ChangerBlocks.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const ChangerBlocks = (params) => {
+  let getClass = (isSelected) => {
+    if (isSelected==true) {
+      return `changer-block selected`
+    }
+    return `changer-block`
+  }
+
   return (
-    <div className="changer-block">
-      {params.ele}
+    <div className={getClass(params.isSelected)} >
+      {params.name}
     </div>
   )
 }
