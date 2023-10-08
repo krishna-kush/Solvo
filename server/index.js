7d90a5ecb30d1d8cfb596cb13df4041de22abe8e
Krishna Kushwaha <krishnakumarkush+1@gmail.com>
2023-10-08 19:39:48 +0530

    ADDED: dot env in server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ import expressWs from 'express-ws'
 // import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 // Mongoose Setup
 import mongoose from 'mongoose';
@@ -11,13 +14,13 @@ import mongoose from 'mongoose';
 // const post_model = require('./models/postMessage.js');
 
 // const CONNECTION_URL = "mongodb+srv://RentMyBook:rmb%40kay+rdj@main.bn0ad.mongodb.net/Books?retryWrites=true&w=majority";
-const CONNECTION_URL = "mongodb+srv://main-pc:main-solvo@main.enaq9qr.mongodb.net/?retryWrites=true&w=majority"; /* Retryable writes allows MongoDB drivers to automatically retry certain write operations a single time if they encounter network errors, or if they cannot find a healthy primary in the replica sets or sharded cluster.
+const CONNECTION_URL = process.env.MONGODB_API_URL; /* Retryable writes allows MongoDB drivers to automatically retry certain write operations a single time if they encounter network errors, or if they cannot find a healthy primary in the replica sets or sharded cluster.
 
 Write concern describes the level of acknowledgment requested from MongoDB for write operations to a standalone mongod or to replica sets or to sharded clusters. In sharded clusters, mongos instances will pass the write concern on to the shards. ( if your replica set has 3 members that mean that your majority=2 , meaning that your write operation will receive acknowledgment after it receive success confirmation from at least 2 members) */
 
-const mongooseLocalhost = 'mongodb://localhost:27017/test'
+const MONGODB_LOCALHOST = process.env.MONGODB_API_URL_LOCAL;
 
-mongoose.connect(mongooseLocalhost, {
+mongoose.connect(MONGODB_LOCALHOST, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
